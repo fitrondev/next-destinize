28425788add76c197d3a4061bd023b2258d1b507
fitronansori <fitronansori7@gmail.com>
2024-06-28 21:28:33 +0800

    update layout component to add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layouts/Header";
 
 const jakarta_sans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -18,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={jakarta_sans.variable}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={jakarta_sans.variable}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
