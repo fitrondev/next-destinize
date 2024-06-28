bb080b78d12e4caf289f477a46cd9e96f7f825c0
fitronansori <fitronansori7@gmail.com>
2024-06-28 21:26:55 +0800

    add navlinks data

---
+++ src/constants/data.ts
@@ -0,0 +1,22 @@
+export const navLinksData = [
+  {
+    href: "/",
+    name: "Home",
+  },
+  {
+    href: "/program",
+    name: "Program",
+  },
+  {
+    href: "/blog",
+    name: "Blog",
+  },
+  {
+    href: "/about",
+    name: "Tentang Kami",
+  },
+  {
+    href: "/kontak",
+    name: "Kontak",
+  },
+];
