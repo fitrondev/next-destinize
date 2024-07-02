7846ccdec93a124c75ad6359a9f6f0d94402d2df
fitronansori <fitronansori7@gmail.com>
2024-07-02 09:59:18 +0800

    update homepage to add company component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Company from "@/components/layouts/Company";
 import Hero from "@/components/layouts/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Company />
     </>
   );
 }
